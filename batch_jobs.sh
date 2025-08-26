99e97dafe393629876646d0dc23a36fec72836ca
George Kouros <george.kouros.ece@gmail.com>
2025-08-26 18:22:35 +0200

    Update scripts

--- batch_jobs.sh
+++ batch_jobs.sh
@@ -8,9 +8,12 @@ echo $DIR
 NUM_JOBS=1
 # ARGS=""# condor_send --jobname "rflgs" --queue "$NUM_JOBS" --conda-env 'ref-gaussian' --gpumem 16 --mem 16 --gpus 1 --cpus 4 --timeout 2.99 --nice 1 -c "bash train.sh"
 # condor_send --jobname "rflgs" --queue "$NUM_JOBS" --conda-env 'ref-gaussian' --gpumem 16 --mem 16 --gpus 1 --cpus 4 --timeout 2.99 --nice 1 -c "bash train.sh && bash eval.sh"
-condor_send --jobname "rflgs" --queue "$NUM_JOBS" --conda-env 'ref-gaussian' --gpumem 16 --mem 16 --gpus 1 --cpus 4 --timeout 2.99 --nice 1 -c "bash eval.sh"
+# condor_send --jobname "rflgs" --queue "$NUM_JOBS" --conda-env 'ref-gaussian' --gpumem 16 --mem 16 --gpus 1 --cpus 4 --timeout 2.99 --nice 1 -c "bash eval.sh"
 
 # condor_send --jobname "rflgs" --queue "$NUM_JOBS" --conda-env 'ref-gaussian' --gpumem 16 --mem 32 --gpus 1 --cpus 4 --timeout 2.99 --nice 1 -c "bash train_shiny.sh"
 # condor_send --jobname "rflgs" --queue "$NUM_JOBS" --conda-env 'ref-gaussian' --gpumem 16 --mem 32 --gpus 1 --cpus 4 --timeout 2.99 --nice 1 -c "bash train_glossy.sh"
 # condor_send --jobname "rflgs" --queue "$NUM_JOBS" --conda-env 'ref-gaussian' --gpumem 24 --mem 32 --gpus 1 --cpus 4 --timeout 2.99 --nice 1 -c "bash train_real.sh"
 # condor_send --jobname "rflgs" --queue "$NUM_JOBS" --conda-env 'ref-gaussian' --gpumem 16 --mem 32 --gpus 1 --cpus 4 --timeout 2.99 --nice 1 -c "bash train_nerf.sh"
+
+
+condor_send --jobname "rflgs" --queue "$NUM_JOBS" --conda-env 'ref-gaussian' --gpumem 16 --mem 16 --gpus 1 --cpus 4 --timeout 0.2 --nice 1 -c "bash test.sh"
